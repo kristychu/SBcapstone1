208606beed19f0d5bca1929493b06e20a69e6fdc
Kristy Chu <kristychu@kristys-mbp.lan>
2020-10-20 14:08:07 -0700

    add login and register routes and methods, install flask-wtf, flask-bcrypt, validators, etc.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, flash, redirect, session
+from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
 
 from models import db, connect_db, User, Fish
+from forms import UserAddForm, LoginForm
+
+CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///acnhcreatures'
@@ -16,3 +20,108 @@ db.create_all()
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+##############################################################################
+# User register/login/logout
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+@app.route('/register', methods=["GET", "POST"])
+def signup():
+    """Handle user registration.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.register(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                profile_img=form.profile_img.data or User.profile_img.default.arg,
+            )
+            db.session.commit()
+
+        except IntegrityError:
+            flash("Username already taken", 'danger')
+            return render_template('users/register.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/register.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+
+    do_logout()
+    flash("You have successfully logged out!", "success")
+    return redirect('/login')
+
+##############################################################################
+# Homepage and error pages
+
+
+@app.route('/')
+def homepage():
+    """Show homepage."""
+
+    if g.user:
+        return render_template('home.html')
+
+    else:
+        return render_template('home-anon.html')
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 """Models for ACNH Creature Tracker app."""
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -27,6 +29,45 @@ class User(db.Model):
                 nullable=False)
     profile_img = db.Column(db.Text,
                 default="/static/images/default-pic.png")
+
+    @classmethod
+    def register(cls, username, email, password, profile_img):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            username=username,
+            email=email,
+            password=hashed_pwd,
+            profile_img=profile_img,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
 
 class Fish(db.Model):
     """Fish."""
